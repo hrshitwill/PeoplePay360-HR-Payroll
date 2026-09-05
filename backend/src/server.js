73993b67ae61fdfbb5b2d9af124baeaf0feff5f5
harshit <hashit@hashits-MacBook-Air.local>
2026-09-05 11:01:52 +0530

    Add backend base setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
+
+// Connect MongoDB
+connectDB();
 
 // Middleware
 app.use(cors());
@@ -16,8 +21,7 @@ app.get("/api/health", (req, res) => {
     });
 });
 
-// Port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
